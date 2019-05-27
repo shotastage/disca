138100b481bc4190ea7224342c4ec4c3cbd56dae
Shota Shimazu <hornet.live.mf@gmail.com>
2019-05-27 16:49:36 +0900

    revise

---
+++ disca-front/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Navbar } from 'react-bootstrap';
+
+
+const DSNavbar = styled(Navbar)`
+    background: #fff !important;
+    filter: drop-shadow(1px 2px 3px rgba(0, 0, 0, 0.336));
+`;
+
+
+export default DSNavbar;
