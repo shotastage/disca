69653ecc1eabf44f082db3aa42818acc91c5b1da
Shota Shimazu <hornet.live.mf@gmail.com>
2019-05-29 00:51:47 +0900

    revise

--- disca-front/src/App.js
+++ disca-front/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavbarWidget } from './components/Navbar';
 import { DSCard, DSCardTitle } from './components/Card';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import DestyledLink from './components/DestyledLink';
 import BgSymbol from './components/BackgroundSymbol';
+
 
 
 function App() {
@@ -17,16 +18,15 @@ function App() {
             <DestyledLink to="/team/">
               <DSCard body>
                 <DSCardTitle>NECO</DSCardTitle>
-                <p>
-                  Description text is here!
-                </p>
+                <Button variant="primary">Primary</Button>
+
               </DSCard>
             </DestyledLink>
           </Col>
           <Col md={3}>
             <DestyledLink to="/team/">
               <DSCard body>
-                <DSCardTitle>データーベース概論</DSCardTitle>
+                <DSCardTitle>NECO</DSCardTitle>
                 <p>
                   Description text is here!
                 </p>
@@ -47,6 +47,30 @@ function App() {
             <DestyledLink to="/team/">
               <DSCard body>
                 <DSCardTitle>Django Up!</DSCardTitle>
+                <p>
+                  Description text is here!
+                </p>
+              </DSCard>
+            </DestyledLink>
+          </Col>
+        </Row>
+
+        <Row>
+          
+          <Col md={3}>
+            <DestyledLink to="/team/">
+              <DSCard body>
+                <DSCardTitle>NECO</DSCardTitle>
+                <p>
+                  Description text is here!
+                </p>
+              </DSCard>
+            </DestyledLink>
+          </Col>
+          <Col md={3}>
+            <DestyledLink to="/team/">
+              <DSCard body>
+                <DSCardTitle>データーベース概論</DSCardTitle>
                 <p>
                   Description text is here!
                 </p>
