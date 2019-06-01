1a3465e4ba76319952d1119a62b9598ca5db3264
Shota Shimazu <hornet.live.mf@gmail.com>
2019-06-01 11:00:38 +0900

    update model

--- session/admin.py
+++ session/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Session, Comment
 # Register your models here.
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ('comment',)
--- session/models.py
+++ session/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+
+
+class Session(models.Model):
+    name = models.CharField(max_length = 255)
+    active = models.BooleanField(default=True)
+    belonging_team = models.CharField(max_length = 255)
+    owner = models.CharField(max_length = 255)
+    created_at = models.DateField(default=timezone.now)
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    session_id = models.CharField(max_length = 255)
+    user = models.CharField(max_length = 255, default="Anonymous")
+    like = models.IntegerField(default=0)
+    time = models.DateField(default=timezone.now)
+    archived = models.BooleanField(default=False)
